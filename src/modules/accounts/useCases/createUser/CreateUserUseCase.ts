933c39e04452d8dcd7961b4067879f6fed94765d
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-27 09:39:26 -0300

    fix(User): remove username

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -4,7 +4,6 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
   name: string;
-  username: string;
   password: string;
   email: string;
   driver_license: string;
@@ -19,14 +18,12 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
     password,
     email,
     driver_license,
   }: IRequest): Promise<void> {
     await this.usersRepository.create({
       name,
-      username,
       password,
       email,
       driver_license,
