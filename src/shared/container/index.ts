a692c140a73299da709e03f269ce691ed618f0ad
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-03 17:48:11 -0300

    chore(UserTokens): add repo

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,7 @@ import { container } from "tsyringe";
 
 import "@shared/container/providers";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsesRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { CarImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
