4792c11e1e5158460a63e6f219085749146148e5
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-10-08 08:32:17 -0300

    chore: minor fixes

--- babel.config.js
+++ babel.config.js
@@ -4,17 +4,19 @@ module.exports = {
     "@babel/preset-typescript"
   ],
   plugins: [
-    ["module-resolver",
-    {
-      alias: {
-        "@modules": "./src/modules",
-        "@config": "./src/config",
-        "@shared": "./src/shared",
-        "@utils": "./src/utils"
-      }
-    }],
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@utils": "./src/utils",
+        },
+      },
+    ],
     "babel-plugin-transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
-}
+};
